11c579908ae37fd8eefe5b59e3aa8daeaf2c1374
janildo1 <janildo@outlook.com>
2023-02-02 21:21:09 -0300

    DELETE para Categoria

--- src/main/java/com/janildo/bookstore/service/CategoriaService.java
+++ src/main/java/com/janildo/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.janildo.bookstore.domain.Categoria;
@@ -37,6 +38,12 @@ public class CategoriaService {
 		obj.setNome(obj.getNome());
 		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
+
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
 
 	}
 
