081f5a5e30ff4debe4daca54fafd5b6f0aa32421
janildo1 <janildo@outlook.com>
2023-02-06 11:21:54 -0300

    Listando todos os livros por categoria

--- src/main/java/com/janildo/bookstore/service/LivroService.java
+++ src/main/java/com/janildo/bookstore/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.janildo.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +14,20 @@ public class LivroService {
 
 	@Autowired
 	private LivroRepository repository;
+	
+	@Autowired
+	private CategoriaService categoriaservice;
 
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaservice.findById(id_cat);
+		return repository.findAllByCategoria(id_cat);
+
 	}
 
 }
