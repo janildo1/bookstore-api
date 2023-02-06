15c9ddde2da10d288f18cb7595db3a705169d01a
janildo1 <janildo@outlook.com>
2023-02-06 15:14:24 -0300

    Ajustes finalizados no backend

--- src/main/java/com/janildo/bookstore/domain/Categoria.java
+++ src/main/java/com/janildo/bookstore/domain/Categoria.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
@@ -12,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 public class Categoria implements Serializable {
@@ -21,7 +24,13 @@ public class Categoria implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 100, message = "o campo NOME deve ter entre 3 e 100 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+	@Length(min = 3, max = 200, message = "o campo DESCRIÇÃO deve ter entre 3 e 200 caracteres")
 	private String descricao;
 
 	@OneToMany(mappedBy = "categoria")
--- src/main/java/com/janildo/bookstore/dtos/CategoriaDTO.java
+++ src/main/java/com/janildo/bookstore/dtos/CategoriaDTO.java
@@ -2,15 +2,24 @@ package com.janildo.bookstore.dtos;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.janildo.bookstore.domain.Categoria;
+
+import jakarta.validation.constraints.NotEmpty;
 
 public class CategoriaDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 100, message = "o campo NOME deve ter entre 3 e 100 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+	@Length(min = 3, max = 200, message = "o campo DESCRIÇÃO deve ter entre 3 e 200 caracteres")
 	private String descricao;
-
 	public CategoriaDTO() {
 		super();
 		// TODO Auto-generated constructor stub
